22ea57c93ee75b5ebc170c0ed015d163f2c8dfef
Roberto Orgiu <roberto.orgiu@gmail.com>
2016-03-02 16:42:49 +0100

    Adds solution for step1

--- app/src/main/java/crunching/rxandroid/workshop/MainActivity.java
+++ app/src/main/java/crunching/rxandroid/workshop/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 import rx.Observable;
 import rx.Subscriber;
+import rx.functions.Action0;
+import rx.functions.Action1;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Playground.create().subscribe(Playground.logStringAction());
+        Playground.just().subscribe(Playground.logStringAction(), Playground.logError());
+        Playground.just().subscribe(Playground.logStringAction(), Playground.logError(), Playground.logCompleted());
+
+        Observable.just("").subscribe(new Action1<String>() {
+            @Override
+            public void call(String s) {
+
+            }
+        }, new Action1<Throwable>() {
+            @Override
+            public void call(Throwable throwable) {
+
+            }
+        }, new Action0() {
+            @Override
+            public void call() {
+
+            }
+        });
     }
 }
--- app/src/main/java/crunching/rxandroid/workshop/Playground.java
+++ app/src/main/java/crunching/rxandroid/workshop/Playground.java
@@ -1,6 +1,57 @@
 package crunching.rxandroid.workshop;
 
+import android.util.Log;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action0;
+import rx.functions.Action1;
+
 public class Playground {
+    private static String TAG = "Playground";
 
+    public static Observable<String> create() {
+        return Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("Hello, World!");
+                subscriber.onCompleted();
+            }
+        });
+    }
 
+    public static Observable<String> just() {
+        return Observable.just("Hello, World!");
+    }
+
+    public static Observable<String> from() {
+        return Observable.from(new String[] {"Hello", "World", "from", "Crunching", "RxAndroid", "Workshop"});
+    }
+
+    public static Action1<String> logStringAction() {
+        return new Action1<String>() {
+            @Override
+            public void call(String s) {
+                Log.d(TAG, s);
+            }
+        };
+    }
+
+    public static Action1<Throwable> logError() {
+        return new Action1<Throwable>() {
+            @Override
+            public void call(Throwable throwable) {
+                Log.e(TAG, throwable.getMessage());
+            }
+        };
+    }
+
+    public static Action0 logCompleted() {
+        return new Action0() {
+            @Override
+            public void call() {
+                Log.d(TAG, "onCompleted()");
+            }
+        };
+    }
 }
