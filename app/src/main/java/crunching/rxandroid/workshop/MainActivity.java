35b0279cf224997b7fdad6a860de8d050b5514be
Roberto Orgiu <roberto.orgiu@gmail.com>
2016-03-07 21:49:25 +0100

    Adds solution for step 2

--- app/src/main/java/crunching/rxandroid/workshop/MainActivity.java
+++ app/src/main/java/crunching/rxandroid/workshop/MainActivity.java
@@ -2,9 +2,11 @@ package crunching.rxandroid.workshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import rx.Observable;
 import rx.Subscriber;
+import rx.functions.Action1;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Playground.getObservable().subscribe(new Action1<String>() {
+            @Override
+            public void call(String s) {
+                Log.d(Playground.TAG, s);
+            }
+        });
     }
 }
--- app/src/main/java/crunching/rxandroid/workshop/Playground.java
+++ app/src/main/java/crunching/rxandroid/workshop/Playground.java
@@ -1,6 +1,28 @@
 package crunching.rxandroid.workshop;
 
+import java.util.Locale;
+
+import rx.Observable;
+import rx.functions.Func1;
+
 public class Playground {
 
+    public static String TAG = "Playground";
+
+
+    public static Observable<String> getObservable() {
+        return Observable.range(1, 100)
+                .filter(new Func1<Integer, Boolean>() {
+                    @Override
+                    public Boolean call(Integer i) {
+                        return (i % 2) == 0;
+                    }
+                }).map(new Func1<Integer, String>() {
+                    @Override
+                    public String call(Integer i) {
+                        return String.format(Locale.getDefault(), "Emitted %d", i);
+                    }
+                }).take(10);
+    }
 
 }
