67560663a3aa03bf2c1dcecdf35f897cfa3006f3
Roberto Orgiu <roberto.orgiu@gmail.com>
2016-03-07 21:57:17 +0100

    Adds solution for step 3

--- app/src/main/java/crunching/rxandroid/workshop/Playground.java
+++ app/src/main/java/crunching/rxandroid/workshop/Playground.java
@@ -1,6 +1,15 @@
 package crunching.rxandroid.workshop;
 
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
 public class Playground {
 
+    public static Observable<String> getChangeTextObservable() {
+        return Observable.just("I changed the value")
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
 
 }
